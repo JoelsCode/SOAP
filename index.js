533777cad4eb1d6f3e9ff2b1fd68ff9367274170
JoelsCode <joelcedano@live.com>
2017-09-22 15:42:12 -0400

    Cleaned up code

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 var Twit = require('twit')
-
-var MY_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"; //#crawlerhistory webhooot
 var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
 var sentiment = require('sentiment');
-
-
-// var Twit = new Twit({
-//     consumer_key: 'Kal8kSl4lGFsTbj8nHHTtCkQg',
-//     consumer_secret: 'XEwUdCEn5RvC43xubbe7FrIGW5fUDKMBjbA2d8Z3OCNWMXj6ue',
-//     access_token: '269905794-K9rJrVbTOENxf8m073PIWFG4slz6owlnYgpcJmi2',
-//     access_token_secret: 'xJAtj9noVWgjER7XeJxOKAM1kTnfYlpeHxsOXwTXRt7Io',
-//     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-// })
-
-// var stream = Twit.stream('statuses/filter', { track: ["Addigy", "addigy", "jamf", "Jamf"], language: 'en' })
-
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-// })
-
 var Twitter = require('node-tweet-stream')
+var MY_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"; //#crawlerhistory webhooot
 
 
 var tstream = new Twitter({
@@ -28,7 +11,7 @@ var tstream = new Twitter({
     token: '269905794-K9rJrVbTOENxf8m073PIWFG4slz6owlnYgpcJmi2',
     token_secret: 'xJAtj9noVWgjER7XeJxOKAM1kTnfYlpeHxsOXwTXRt7Io'
 })
-tstream.trackMultiple("dog");
+tstream.trackMultiple(["MacOS"]);
 
 tstream.on("tweet",function(tweet){
     
